bc61dfc075d38a89c7743a094a8fe300873a5bba
Kant <84694727+webby-Kant@users.noreply.github.com>
2021-07-06 18:13:12 +0530

    Create py_project3.py
    For accepting an integer from user and checking if this integer is greater than 10.

---
+++ py_project3.py
@@ -0,0 +1,7 @@
+#For accepting an integer from user and checking if this integer is greater than 10.
+
+integer = int(input("enter integer: "))
+if integer > 10:
+    print("yes, it is greater then 10.")
+else:
+    print("no, it is not smaller than 10")
