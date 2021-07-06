3e1539bc06129314aa29d0de8540d6e26430beb9
Kant <84694727+webby-Kant@users.noreply.github.com>
2021-07-06 18:10:14 +0530

    Create py_project1.py
    To find out the area and perimeter of rectangle.

---
+++ py_project1.py
@@ -0,0 +1,9 @@
+#To find out the area and perimeter of rectangle.
+
+width = int(input("enter width(in meters): "))
+length = int(input("enter length(in meters): "))
+area_of_rectangle = width * length
+print("area: ",area_of_rectangle, "m^2")
+
+peremeter_of_rect = 2*(length+width)
+print("perimeter: ", peremeter_of_rect, "m")
