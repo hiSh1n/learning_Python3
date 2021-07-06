b594a4b7b03112ebccdea6d687b4108d2aa155e8
Kant <84694727+webby-Kant@users.noreply.github.com>
2021-07-06 18:14:23 +0530

    Create py_project4.py
    To find out the greatest number from any two numbers.

---
+++ py_project4.py
@@ -0,0 +1,8 @@
+#To find out the greatest number from any two numbers.
+
+first_no = int(input("enter first no. "))
+second_no = int(input("enter second no. "))
+if first_no >= second_no:
+    print(first_no, "is greater.")
+else:
+    print(second_no, "is greater.")
