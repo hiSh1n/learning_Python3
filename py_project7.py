04716ce5dfa71a4eda8a9dd3e92cac497dca4465
Kant <84694727+webby-Kant@users.noreply.github.com>
2021-07-06 18:16:25 +0530

    Create py_project7.py
    To display quotient and remainder.

---
+++ py_project7.py
@@ -0,0 +1,7 @@
+#To display quotient and remainder.
+devidend = int(input("enter dividend: "))
+divisor = int(input("enter divisor: "))
+quotient = devidend // divisor
+remainder = devidend % divisor
+print("quotient is ", quotient)
+print("remainder is ", remainder)
